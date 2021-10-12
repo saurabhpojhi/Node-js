0c235046f0e0e780ed4c3cbcfe34e98454b81145
saurabh kumar <35069331+saurabhpojhi@users.noreply.github.com>
2021-10-13 01:00:38 +0530

    Add files via upload

---
+++ httpservermake.js
@@ -0,0 +1,13 @@
+
+const http = require("http");
+
+  const server = http.createServer((request, response, next)=>{
+
+
+    
+});
+server.listen(4000, 'localhot',()=>{
+
+console.log(" Server is working on http://localhost:4000");
+
+});
