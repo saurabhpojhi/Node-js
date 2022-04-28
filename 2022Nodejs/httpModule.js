f2055e272ce46302eea6c5ecf738e77fcdfdebfb
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-04-29 01:02:53 +0530

    create server

--- 2022Nodejs/httpModule.js
+++ 2022Nodejs/httpModule.js
@@ -1,3 +1,11 @@
-// it is used 
+// it is used  create server 
 
 const http = require("http");
+
+const server = http.createServer(function(req,res){
+
+    res.writeHead(200,{"content-type":"test/plain"});
+    res.write("node js coding")    
+    res.end();
+
+}).listen(3000,()=>{console.log("server running on port 3000")});
