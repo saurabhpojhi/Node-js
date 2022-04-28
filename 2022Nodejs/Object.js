ade5b7af590be682ca786e94d330b6d146954001
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-04-28 23:45:36 +0530

    object

---
+++ 2022Nodejs/Object.js
@@ -0,0 +1,11 @@
+
+// making object or creat object
+
+var user ={name:"saurabh",age:28,email:"saurabh.k@gmail.com"}    // making object or creat object
+
+console.log("user",user);
+console.log(user.name);    // here object define
+console.log(user.age);
+
+// Anonymos function
+
