786b35b39ddbc4f330cab3cc8ed79d1e527579ff
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-05-05 00:02:00 +0530

    add

--- osmodule.js
+++ osmodule.js
@@ -6,6 +6,6 @@ console.log(os.homedir())
 console.log(os.hostname())
 
 console.log(os.release())
-console.log(os.platform))
+console.log(os.platform)
 console.log(os.uptime())
 console.log(os.type())
